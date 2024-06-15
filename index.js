a4a807d5dc9304f42b33645110b2014499fde075
MrDhruvchandra <dhruvmaurya298@gmail.com>
2024-06-15 17:15:18 +0530

    backend connented to database

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express =require("express");
 const app =express();
+const {todo}=require("./db.js")
 const { creatPayload, updateTodo}=require("./types.js")
 port= 3000;
 app.use(express.json());
-app.get("/todo",function(req,res){
+app.get("/todo",async function(req,res){
+    const todos= await todo.find({});
+    res.json({
+        todos
+    })
+
+       
+    // it's hitting the data base so it should be promise
 
 })
-app.post("/todos",function(req,res){
+app.post("/todos",async function(req,res){
 const creatPayload=req.body;
 const parsePayload=createTodo.safeParse(creatPayload);
 if(!parsePayload.success){
@@ -14,8 +22,19 @@ if(!parsePayload.success){
         msg:"you entered wrong credentials"
     })
 }
+// connect to mongodb
+await todo.create({
+    title: creatPayload.title,
+    description:creatPayload.description,
+    completed:false
 })
-app.put("/completed",function(req,res){
+res.json({
+    msg:"todo created successfully"
+})
+})
+
+
+app.put("/completed",async function(req,res){
 const createPayload=req.body;
 const parsePayload=updateTodo.safeParse(updateTodo);
 if(!parsePayload.success){
@@ -23,6 +42,12 @@ res.status(411).json({
     msg:"you entered wrong credentials"
 })
 }
+await todo.update({_id:req.body.id},{completed:true
+
+})
+res.json({
+    msg:"todo is marked as completed"
+})
 })
 
 
