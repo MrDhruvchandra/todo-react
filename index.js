cc0669c12edca758b1e4aa7d7540a9b403d41266
MrDhruvchandra <dhruvmaurya298@gmail.com>
2024-06-15 16:40:45 +0530

    second commit

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express =require("express");
 const app =express();
+const { creatPayload, updateTodo}=require("./types.js")
 port= 3000;
 app.use(express.json());
-app.get("/todo",function(req,res)=>{
+app.get("/todo",function(req,res){
 
 })
-app.post("/todos",function(req,res)=>{
-
+app.post("/todos",function(req,res){
+const creatPayload=req.body;
+const parsePayload=createTodo.safeParse(creatPayload);
+if(!parsePayload.success){
+    res.status(411).json({
+        msg:"you entered wrong credentials"
+    })
+}
 })
-app.put("/completed",function(req,res)=>{
-
+app.put("/completed",function(req,res){
+const createPayload=req.body;
+const parsePayload=updateTodo.safeParse(updateTodo);
+if(!parsePayload.success){
+res.status(411).json({
+    msg:"you entered wrong credentials"
 })
+}
+})
+
 
 app.listen(port);
